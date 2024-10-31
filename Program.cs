7c43f97d6cd6f2facb23a5944157b4dff7f50098
Luis Coco Enríquez <LEnriquez@luxoft.com>
2024-10-31 10:45:12 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using OpenAI;
+using Microsoft.Extensions.AI;
+
+IChatClient client = new OpenAIClient("").AsChatClient("gpt-4o");
+
+var response = await client.CompleteAsync("What is AI?");
+
+Console.WriteLine(response);
+
+
